b7753e7084294aebb835ebed26aa27e271162b58
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2022-12-21 13:25:35 +0530

    Schema, Field Checks added.

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -3,10 +3,42 @@ export default class authController {
     static register(req, res) {
         return res.render('register');
     }
-    static postRegister(req, res) {
-        const { firstName, lastName, email, phone, password, cpassword } =
-            req.body;
-        console.log(firstName, lastName, email, phone, cpassword);
-        return res.send('Success');
+    static async postRegister(req, res) {
+        try {
+            const { firstName, lastName, email, phone, password, cpassword } =
+                req.body;
+            const userExists = await User.exists({ email });
+
+            if (
+                !firstName ||
+                !lastName ||
+                !email ||
+                !phone ||
+                !password ||
+                !cpassword
+            ) {
+                return res.redirect('/register');
+            } else if (password != cpassword) {
+                return res.json({
+                    message: `Passwords doesn't match`,
+                });
+            } else if (userExists) {
+                return res.json({ message: 'Email already taken.' });
+            } else {
+                const registerUser = new User({
+                    firstName,
+                    lastName,
+                    email,
+                    phone,
+                    password,
+                    cpassword,
+                });
+                await registerUser.save();
+                return res.redirect('/');
+            }
+        } catch (error) {
+            res.redirect('/register');
+            console.log(error);
+        }
     }
 }
