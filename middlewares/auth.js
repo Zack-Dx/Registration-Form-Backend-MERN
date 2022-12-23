9a36e4837c3df4abeb41d2e20b59109080c3bd23
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2022-12-23 15:27:14 +0530

    Middleware and Authentication Setup Completed.

---
+++ middlewares/auth.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+import { User } from '../models/user.js';
+import dotenv from 'dotenv';
+dotenv.config();
+const auth = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+        const verifyUser = jwt.verify(token, process.env.mySecret);
+        const user = await User.findOne({ _id: verifyUser });
+        next();
+
+        // next();
+    } catch (error) {
+        res.status(401).redirect('/login');
+    }
+};
+export default auth;
--- routes/web.js
+++ routes/web.js
@@ -2,9 +2,10 @@ import express from 'express';
 import homeController from '../controllers/homeController.js';
 import registerController from '../controllers/auth/registerController.js';
 import loginController from '../controllers/auth/loginController.js';
+import auth from '../middlewares/auth.js';
 const router = express.Router();
 // Routing
-router.get('/', homeController.index);
+router.get('/', auth, homeController.index);
 router.get('/register', registerController.register);
 router.post('/register', registerController.postRegister);
 router.get('/login', loginController.login);
